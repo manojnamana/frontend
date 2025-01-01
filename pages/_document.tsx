3196d4f84468bd0e52858826e732fff83b128e1f
manojnamana <namanamanoj70@gmail.com>
2025-01-01 13:50:59 +0530

    second commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
           />
         </Head>
-      <body>
+      <body style={{backgroundColor:" #efe8fd;"}}>
         <Main />
         <NextScript />
       </body>
